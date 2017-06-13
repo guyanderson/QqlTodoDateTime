ea422dce4cd162a7dc73fc613abbb009f19336b2
Guy <luckythelich@hotmail.com>
2017-06-13 15:36:45 -0700

    formatted WLs

--- Tests/CategoryTest.cs
+++ Tests/CategoryTest.cs
@@ -15,7 +15,7 @@ namespace ToDoList
     }
 
     [Fact]
-    public void Test_CategoriesEmptyAtFirst()
+    public void GetAll_CategoriesEmptyAtFirst_True_1()
     {
       //Arrange, Act
       int result = Category.GetAll().Count;
@@ -24,19 +24,19 @@ namespace ToDoList
       Assert.Equal(0, result);
     }
     [Fact]
-    public void Equal_ReturnsTrueForSameName_True()
+    public void Equal_ReturnsTrueForSameName_True_2()
     {
       //Arrange, Act
       Category firstCategory = new Category("Household chores");
       Category secondCategory = new Category("Household chores");
-Console.WriteLine(firstCategory.GetName());
-Console.WriteLine(secondCategory.GetName());
+Console.WriteLine(firstCategory.GetName()+"  FC2");
+Console.WriteLine(secondCategory.GetName()+"  SC2");
       //Assert
       Assert.Equal(firstCategory, secondCategory);
     }
 
     [Fact]
-    public void Save_SavesToDatabase_True()
+    public void Save_SavesToDatabase_True_3()
     {
       //Arrange, Act
       Category testCategory = new Category("Garden");
@@ -45,27 +45,27 @@ Console.WriteLine(secondCategory.GetName());
       //Act
       List<Category> result = Category.GetAll();
       List<Category> testList = new List<Category>{testCategory};
-Console.WriteLine(result[0].GetName());
-Console.WriteLine(testList[0].GetName());
+Console.WriteLine(result[0].GetName()+"  R3");
+Console.WriteLine(testList[0].GetName()+" TL3");
       //Assert
       Assert.Equal(testList, result);
     }
 
     [Fact]
-    public void Find_FindsCategoryInDatabase_True()
+    public void Find_FindsCategoryInDatabase_True_4()
     {
       Category testCategory = new Category("Laundry");
       testCategory.Save();
 
       Category foundCategory = Category.Find(testCategory.GetId());
-Console.WriteLine(testCategory.GetName());
-Console.WriteLine(foundCategory.GetName());
+Console.WriteLine(testCategory.GetName()+"  TC4");
+Console.WriteLine(foundCategory.GetName()+"  FC4");
 
       Assert.Equal(testCategory, foundCategory);
     }
 
     [Fact]
-    public void Update_UpdatesCategoryInDatabase_True()
+    public void Update_UpdatesCategoryInDatabase_True_5()
     {
       string name = "Home stuff";
       Category testCategory = new Category(name);
@@ -75,13 +75,13 @@ Console.WriteLine(foundCategory.GetName());
       testCategory.Update(newName);
 
       string result = testCategory.GetName();
-//Console.WriteLine(result.GetName());
-//Console.WriteLine(newName.GetName());
+Console.WriteLine(result + "  R5");
+Console.WriteLine(newName + "  NN5");
       Assert.Equal(newName, result);
     }
 
     [Fact]
-    public void AddTask_AddsTaskToCategory_True()
+    public void AddTask_AddsTaskToCategory_True_6()
     {
       //Arrange
       Category testCategory = new Category("Household chores");
@@ -99,15 +99,15 @@ Console.WriteLine(foundCategory.GetName());
 
       List<Task> result = testCategory.GetTasks();
       List<Task> testList = new List<Task>{testTask, testTask2};
-Console.WriteLine(result[0].GetDescription());
-Console.WriteLine(testList[0].GetDescription());
+Console.WriteLine(result[0].GetDescription()+"  R6");
+Console.WriteLine(testList[0].GetDescription()+"  TL6");
 
       //Assert
       Assert.Equal(testList, result);
     }
 
     [Fact]
-    public void AddTask_ReturnsAllCategoryTasks_True()
+    public void AddTask_ReturnsAllCategoryTasks_True_7()
     {
       //Arrange
       Category testCategory = new Category("Household chores");
@@ -123,8 +123,8 @@ Console.WriteLine(testList[0].GetDescription());
       testCategory.AddTask(testTask1);
       List<Task> savedTasks = testCategory.GetTasks();
       List<Task> testList = new List<Task> {testTask1};
-Console.WriteLine(savedTasks[0].GetDescription());
-Console.WriteLine(testList[0].GetDescription());
+Console.WriteLine(savedTasks[0].GetDescription()+"  ST7");
+Console.WriteLine(testList[0].GetDescription()+"  TL7");
       //Assert
       Assert.Equal(testList, savedTasks);
     }
